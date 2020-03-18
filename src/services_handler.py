99f0f823fb3ff0d0d0a2a2638b446bee283dc9bf
Aitor Iturrioz <riturrioz@gmail.com>
2020-03-18 12:31:16 +0100

    Handle SSH service
    Signed-off-by: Aitor Iturrioz <riturrioz@gmail.com>

--- src/services_handler.py
+++ src/services_handler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from enum import Enum
 import logging
 import subprocess
 import sys
@@ -7,12 +8,19 @@ import webbrowser
 
 logger = logging.getLogger(__name__)
 
+class OS(Enum):
+    Linux = "linux"
+    Windows = "win32"
+    MacOS = "darwin"
+
 def launch_service(info):
     type = info["type"]
     if type == "web":
         return launch_service_web(info)
     elif type == "smb":
         return launch_service_smb(info)
+    elif type == "ssh":
+        return launch_service_ssh(info)
     else:
         logger.warn("Unkwnon service: {}".format(type))
     return
@@ -28,19 +36,43 @@ def launch_service_web(info):
 def launch_service_smb(info):
     ip = info["ip"]
     uri = "smb://{}".format(ip)
-    if sys.platform == "linux":
+    if sys.platform == OS.Linux.value:
         logger.debug("Openning smb folder: '{}'".format(uri))
         subprocess.Popen(["xdg-open", uri])
         return True
-    elif sys.platform == "win32":
+    elif sys.platform == OS.Windows.value:
         uri = "//{}".format(ip)
         logger.debug("Openning smb folder: '{}'".format(uri))
         subprocess.Popen('explorer "{0}"'.format(uri))
         return True
-    elif sys.platform == 'darwin':
+    elif sys.platform == OS.MacOS.value:
         logger.debug("Openning smb folder: '{}'".format(uri))
         subprocess.Popen(["open", uri])
         return True
     else:
         logger.warn("Unknown system platform: {}".format(sys.platform))
     return
+
+def launch_service_ssh(info):
+    ip = info["ip"]
+    port = info["port"]
+    user = info["user"]
+    if sys.platform == OS.Linux.value:
+        # Check gnome-terminal
+        if __check_command("gnome-terminal"):
+            command = "ssh -p {} {}@{}".format(port, user, ip)
+            subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', command])
+            return True
+        # TO-DO: Add more terminal applications (Konsole...)
+    elif sys.platform == OS.Windows.value:
+        # TO-DO: Implement it on Windows (with PuTTy?)
+        pass
+    elif sys.platform == OS.MacOS.value:
+        # TO-DO: Implement it on MacOS
+        pass
+    else:
+        logger.warn("Unknown system platform: {}".format(sys.platform))
+    return
+
+def __check_command(name):
+    return not subprocess.call(["which", name])
